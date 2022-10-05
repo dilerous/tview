fe3e3448dd119f58338e3d4844b6dbcb440dfa11
Brad Soper <brad.soper@gmail.com>
2022-10-05 11:25:26 -0700

    removed unused code which was moved to ui.go

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ import (
 )
 
 var (
-	ctx      = context.Background()
-	cli, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	ctx    = context.Background()
+	cli, _ = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 )
 
 var (
@@ -51,82 +51,8 @@ func init() {
 
 func main() {
 
-	//i := Images{}
 	runTview()
-	//mainMenu(&i)
-
-	/*
-		text.SetBorder(true)
-
-		pages.AddPage("Menu", menu, true, true).
-			AddPage("View", text, true, false).
-			AddPage("Push", form, true, false).
-			SetBorder(true)
-
-		flex.AddItem(pages, 0, 4, true).
-			AddItem(text, 0, 4, false)
-
-		flex.SetDirection(tview.FlexRow)
-
-		flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-			if event.Rune() == 113 {
-				app.Stop()
-			} else if event.Rune() == 27 {
-				form.Clear(true)
-				pages.SwitchToPage("Menu")
-				app.SetFocus(menu)
-			}
-			return event
-		})
-	*/
-
 }
-
-/*
-	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
-		log.Println(err)
-		panic(err)
-	}
-}
-*/
-
-/*
-func (i *Images) mainMenu() {
-
-	i.username = "cnvrghelm"
-
-	menu.SetBorder(true).
-		SetTitle(" cnvrg.io Deployment Tool ").
-		SetTitleAlign(tview.AlignCenter).
-		SetTitleColor(tcell.ColorGreen)
-
-	text.SetText("Please enter the Docker Hub credientials provided by cnvrg.io to download the images needed.").
-		SetWordWrap(true).
-		SetTextColor(tcell.ColorWhite)
-
-	menu.AddInputField("cnvrg.io Docker Username: ", "cnvrghelm", 40, nil, func(user string) {
-		i.username = user
-	}).AddPasswordField("cnvrg.io Docker Password: ", "", 40, 42, func(password string) {
-		i.password = password
-	}).AddInputField("Images File: ", "", 40, nil, func(fileName string) {
-		i.fileName = fileName
-	}).AddButton("Quit", func() {
-		app.Stop()
-	}).AddButton("View File", func() {
-		f, err := readFile(i.fileName)
-		updateText(f, err)
-	}).AddButton("Pull Images", func() {
-		f, _ := readFile(i.fileName)
-		text.Clear()
-		i.pullImages(f)
-	}).AddButton("Push Images", func() {
-		f, _ := readFile(i.fileName)
-		i.dockerPush(f)
-		pages.SwitchToPage("Push")
-	})
-
-}
-*/
 
 // s []string is source image.
 // t string is the target which is pulled from registry input field.
@@ -254,11 +180,7 @@ func (i *Images) pullImages(s []string) {
 	for _, v := range s {
 		i.streamPullToWriter(v, cli)
 	}
-	/*
-		text.SetText("Success! All Images downloaded successfully. Check logs for details.").
-			SetTextColor(tcell.ColorGreen).
-			SetWordWrap(true)
-	*/
+
 }
 
 // takes images as a string and streams the update to text
@@ -293,45 +215,6 @@ func (i *Images) streamPullToWriter(s string, c *client.Client) {
 		log.Println(text)
 	}()
 }
-
-/*
-func (i *Images) dockerPush(s []string) {
-	i.server = "docker.io"
-
-	form.SetBorder(true).
-		SetTitle(" cnvrg.io Deployment Tool ").
-		SetTitleAlign(tview.AlignCenter).
-		SetTitleColor(tcell.ColorGreen)
-
-	text.SetText("Please enter the private registry credientials to push images.").
-		SetWordWrap(true).
-		SetTextColor(tcell.ColorWhite)
-
-	form.AddInputField("Docker Username: ", "", 40, nil, func(user string) {
-		i.username = user
-	}).AddPasswordField("Docker Password: ", "", 40, 42, func(password string) {
-		i.password = password
-	}).AddInputField("Server Address: ", "docker.io", 40, nil, func(server string) {
-		i.server = server
-	}).AddInputField("Registry: ", "", 40, nil, func(registry string) {
-		i.registry = registry
-	}).AddButton("Return to Main Menu", func() {
-		form.Clear(true)
-		pages.SwitchToPage("Menu")
-		app.SetFocus(menu)
-	}).AddButton("Tag Images", func() {
-		text.Clear()
-		f, _ := readFile(i.fileName)
-		i.tagImages(f)
-	}).AddButton("Push to Registry", func() {
-		text.Clear()
-		i.pushImages()
-	}).AddButton("List Images", func() {
-		text.Clear()
-		listImages()
-	})
-}
-*/
 
 func handlePanic(err interface{}) {
 	InfoLogger.Println("In the handlePanic function")
@@ -371,3 +254,9 @@ func readFile(f string) ([]string, error) {
 	}
 	return images, err
 }
+
+/*
+	text.SetText("Success! All Images downloaded successfully. Check logs for details.").
+		SetTextColor(tcell.ColorGreen).
+		SetWordWrap(true)
+*/
